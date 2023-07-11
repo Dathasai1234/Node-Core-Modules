3952e1b18b817a4e1105a8bfc21ae3263dc8464f
Datha sai <dathasaichandu@gmail.com>
2023-07-11 17:42:16 +0530

    routing Requests

--- first-app.js
+++ first-app.js
@@ -29,8 +29,15 @@ const http = require('http');
 //! method 3 next Gen function
 
 const server = http.createServer((req, res) => {  // Create a server using the http.createServer() method.
-    console.log(req.url, req.headers, req.method);  // Log the URL, headers, and method of the incoming request.
-    // process.exit();     // stops the eventloop and exits the server
+    const url = req.url;
+    if(url === '/') {
+        res.write('<html>');  // Write the opening html tag to the response.
+        res.write('<head><title>Enter Message</title></head>');  // Write the head section of the HTML.
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send<button/><form/></body>');  // Write the body section of the HTML.
+        res.write('</html>');  // Write the closing html tag to the response.
+    
+        return res.end();  // End the response and send it to the client.
+    }
     
     res.setHeader('content-Type', 'text/html');  // Set the content type of the response to 'text/html'.
 
@@ -68,3 +75,20 @@ server.listen(3000);
 //? Then, we use `res.write()` method to write HTML content to the response. We start with the opening `<html>` tag, then add the `<head>` section with a title, and finally the `<body>` section with a heading.
 
 //? Finally, we call `res.end()` to end the response and send it to the client.
+
+//***************************************************
+
+
+//? In the HTML code you provided, the action attribute and the POST method have the following meanings:
+
+//? action attribute: This attribute specifies the URL or endpoint where the form data will be submitted to. In your example, the action attribute is set to /message. When the form is submitted, the data will be sent to the /message URL on the server-side.
+
+//? method attribute: This attribute specifies the HTTP method that will be used to send the form data. In your example, the method attribute is set to POST. This means that when the form is submitted, the data will be sent to the server using a POST request.
+
+//? So, when a user fills in the form and clicks the "Send" button, the browser will send a POST request to the server at the /message URL, including the data from the input field named "message" in the request body. The server can then process the data sent in the POST request and perform any necessary actions based on the received data.
+
+//***************************************************
+
+//? In your HTML code snippet, the name attribute is used in the <input> element. The name attribute specifies the name or key that will be associated with the value entered in the input field. In this case, the input field has the name "message". When the form is submitted, the data entered in the input field will be sent to the server with the name "message" as the key and the corresponding value as the user input.
+
+//? The name attribute is important because it allows you to access the submitted data on the server-side. When the form is submitted, you can retrieve the value of the input field named "message" from the request body on the server and perform further processing or store it in a database, for example.
