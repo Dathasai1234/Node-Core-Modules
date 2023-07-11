219df40e217b6d6b703348fc4e3d61744a6fd954
Datha sai <dathasaichandu@gmail.com>
2023-07-11 10:26:29 +0530

    createServer method

--- first-app.js
+++ first-app.js
@@ -1,5 +1,37 @@
-console.log("Hello from Node.js");
+//! Creating a fully functional web server.
 
-const fs = require("fs");
+const http = require('http');
 
-fs.writeFileSync("hello.txt", "Hello from Node.js");
+//! method 1
+
+// function rqListener(req, res) {
+
+// }
+
+// http.createServer(rqListener);
+
+//***************************************************
+//! method 2 Anoynomous function
+
+// http.createServer(function(req, res) {
+    
+// });
+
+//***************************************************
+/*
+    If X happen, Y should happen
+
+    X - request comes
+    Y - Please execute these functions
+*/
+
+//***************************************************
+//! method 3 next Gen function
+
+const server = http.createServer((req, res) => {
+    console.log(req);
+});
+
+// stored in a constant variable as the createserver methord returns an instance of a server.
+
+server.listen(3000);
