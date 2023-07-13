0dc7d63046fd4473503cec5c436778c4348dce21
Datha sai <dathasaichandu@gmail.com>
2023-07-13 10:10:21 +0530

    expressjs helloworld

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-const http = require('http');
+const express = require('express');
 
-const server = http.createServer();
+const app = express();
 
-server.listen(3000);
+app.use('/', (req, res, next) => {
+    console.log("This works everytime");
+    next();
+});
+
+app.use('/add-product', (req, res, next) => {
+    console.log("This is a middleware");
+    res.send('<h1>This is add-product path</h1>');
+});
+
+app.use('/', (req, res, next) => {
+    console.log("This is another middleware");
+    res.send('<h1>This is url path</h1>');
+}); 
+
+app.listen(3000);
