33c66386c90a920187b6fad7276eb4d977bb63b5
Datha sai <dathasaichandu@gmail.com>
2023-07-17 20:48:44 +0530

    Adding a 404 error page

--- app.js
+++ app.js
@@ -15,4 +15,8 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(adminroutes);
 app.use(shoproutes);
 
-app.listen(3000);
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+})
+
+app.listen(5000);
