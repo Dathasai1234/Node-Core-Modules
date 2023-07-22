d77bf1a8b2e6962838dd6437f6a515cbc4593250
Datha sai <dathasaichandu@gmail.com>
2023-07-22 21:03:09 +0530

    Handlebars layouts

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin'); 
 const shoproutes = require('./routes/shop'); 
@@ -10,10 +11,25 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+//***************************************************
+//! PUG
+
 //! to set values globally on our express application
 //! So now we're telling express that we want to compile dynamic templates with the pug engine and 
-app.set('view engine', 'pug');
+// app.set('view engine', 'pug');
 //! where to find these templates.
+// app.set('views', 'views');
+
+//***************************************************
+//! HandleBars
+
+app.engine(
+    'hbs', 
+    expressHbs({
+        layoutsDir: 'views/layouts/', 
+        defaultLayout: 'main-layout', 
+        extname: 'hbs'}));
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 ///***************************************************
@@ -29,4 +45,4 @@ app.use((req, res, next) => {
     res.status(404).render('page-not-found', {docTitle : '404'})
 })
 
-app.listen(3000);
+app.listen(5000);
--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,13 @@ const products = [];
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
     // res.sendFile(path.join(rootdir, 'views', 'add-product.html'));
-    res.render('add-product', {docTitle: 'add-product', path: '/admin/add-product'});
+    res.render('add-product', {
+        docTitle: 'add-product', 
+        path: '/admin/add-product',
+        formCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+    });
 });
 
 // /admin/add-product => POST
